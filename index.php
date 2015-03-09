9b8df0b30aedd015a44456aaeacced58bc4759dd
delanoister-Andro-ID <delano.erick@gmail.com>
2015-03-09 22:35:08 +0700

    rapikeun

--- index.php
+++ index.php
@@ -4,21 +4,18 @@
  </head>
  <body>
  <?php 
-
-
- 	echo "<h1><font color=#ff0000>Hello World</font></h1>This is just a file testing"; 
-	
-	for($i=1;$i<=10;$i++){
-   echo "Hello World <br>This is just a file testing"; 
+ 	echo "<h1><font color=#ff0000>Hello World</font></h1>"; 
+	for($i=1;$i<=5;$i++){
+   		echo "This is just a file testing<br>"; 
  	}
-	echo "<center><table width=300 border=1>";
+	echo "<table width=300 border=1 align=center>";
 	echo "<tr>";
 	echo "<th colspan=5><div align=center>Klub Kerang Ajaib</div></th>";
 	echo "</tr>";
 	echo "<tr>";
 	echo "<td>Erick</td><td>Fajar</td><td>Ilham</td><td>Candra</td><td>Arifin</td>";
 	echo "</tr>";
-	echo "</table></center>";
+	echo "</table>";
 
   ?> 
  </body>
