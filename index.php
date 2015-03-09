7ccab6886f25dfa9b6005b552ebecfc68f3245ab
alphaenciel <alphaenciel@gmail.com>
2015-03-09 21:08:01 +0700

    nambah tabel kerang ajaib

--- index.php
+++ index.php
@@ -5,6 +5,16 @@
  <body>
  <?php 
  	echo "Hello World <br>This is just a file testing"; 
+	
+	echo "<center><table width=300 border=1>";
+	echo "<tr>";
+	echo "<th colspan=5><div align=center>Klub Kerang Ajaib</div></th>";
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td>Erick</td><td>Fajar</td><td>Ilham</td><td>Candra</td><td>Arifin</td>";
+	echo "</tr>";
+	echo "</table></center>";
   ?> 
  </body>
+ 
 </html>
